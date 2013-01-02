5e4dbc4902200c49d3b219fec576179834d0efdb
shelton <sg.bythelake@gmail.com>
2013-01-02 15:47:57 -0800

    Initial game commit with player on board

---
+++ game.js
@@ -0,0 +1,120 @@
+var canvasBg = document.getElementById("canvasBg"),
+	ctxBg = canvasBg.getContext("2d"),
+	canvasEntities = document.getElementById("canvasEntities"),
+	ctxEntities = canvasEntities.getContext("2d"),
+	canvasWidth = canvasBg.width,
+	canvasHeight = canvasBg.height,
+	player1 = new Player(),
+	//enemies = [],
+	//numEnemies = 5,
+	//obstacles = [],
+	isPlaying = false,
+	requestAnimFrame = 	window.requestAnimationFrame ||
+						window.webkitRequestAnimationFrame ||
+						window.mozRequestAnimationFrame ||
+						window.oRequestAnimationFrame ||
+						window.msRequestAnimationFrame ||
+						function(callback) {
+							window.setTimeout(callback, 1000 / 600);
+						},
+	imgSprite = new Image();
+	imgSprite.src = "images/sprite.png";
+	imgSprite.addEventListener("load", init, false );
+
+
+function init() {
+	//document.addEventListener("keydown", checkKeyDown, false);
+	//document.addEventListener("keyup", checkKeyUp, false);
+	//defineObsticles();
+	//initEnemies();
+	begin();
+}
+
+function begin() {
+	ctxBg.drawImage(imgSprite, 0, 0, canvasWidth, canvasHeight, 0, 0, canvasWidth, canvasHeight);
+	isPlaying = true;
+	requestAnimFrame(loop);
+}
+
+function update() {
+	clearCtx(ctxEntities);
+	//updateAllEnemies();
+	player1.update();
+}
+
+function draw() {
+	//drawAllEnemies();
+	player1.draw();
+}
+
+// The game engine function
+function loop() {
+	if (isPlaying) {
+		update();
+		draw();
+		requestAnimFrame(loop);
+
+	}
+}
+
+function clearCtx(ctx) {
+	ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+}
+
+
+function randomRange (min, max) {
+	return Math.floor(Math,random() * (max + 1 - min)) + min;
+}
+
+
+// Player object
+function Player() {
+	this.srcX = 0;
+	this.srcY = 600;
+	this.width = 35;
+	this.height = 54;
+	this.drawX = 400;
+	this.drawY = 300;
+	this.centerX = this.drawX + (this.width / 2);
+	this.centerY = this.drawY + (this.height / 2);
+	this.speed = 2;
+	//this.isUpKey = false;
+	//this.isRigntKey = fales;
+	//this.isDownKey = false;
+	//this.isLeftKey = false;
+	//this.isSpaceBar = false;
+	//this.isShooting = false;
+	//var numBullets = 10;
+	//this.bullets = [];
+	//this.currentBullets = 0;
+	//for (var i = 0; i < numBullets; i++) {
+	//	this.bullets[this.bullets.length] = new Bullet();
+	//}
+}
+
+Player.prototype.update = function () {
+	this.centerX = this.drawX + (this.width / 2);
+	this.centerY = this.drawY + (this.width / 2);
+	//this.checkDirection();
+	//this.checkShooting();
+	//this.updateAllBullets();
+
+};
+
+Player.prototype.draw = function () {
+	//this.drawAllBullets();
+		ctxBg.drawImage(imgSprite, 0, 0, canvasWidth, canvasHeight, 0, 0, canvasWidth, canvasHeight);
+
+	ctxEntities.drawImage(imgSprite, this.srcX, this.srcY, this.width, this.height, this.drawX, this.drawY, this.width, this.height);
+};
+
+
+
+
+
+
+
+
+
+
+
