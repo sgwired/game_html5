219d344bbdaa721c2b0ab077daacb43f378d6982
Shelton Gayles <sg.bythelake@gmail.com>
2013-01-28 19:26:11 -0800

    Final
    Shooting now works

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ var canvasBg = document.getElementById("canvasBg"),
 	canvasHeight = canvasBg.height,
 	player1 = new Player(),
 	enemies = [],
-	numEnemies = 5,
+	numEnemies = 10,
 	obstacles = [],
 	isPlaying = false,
 	requestAnimFrame = 	window.requestAnimationFrame ||
@@ -15,11 +15,11 @@ var canvasBg = document.getElementById("canvasBg"),
 						window.oRequestAnimationFrame ||
 						window.msRequestAnimationFrame ||
 						function(callback) {
-							window.setTimeout(callback, 1000 / 600);
+							window.setTimeout(callback, 1000 / 60);
 						},
 	imgSprite = new Image();
 	imgSprite.src = "images/sprite.png";
-	imgSprite.addEventListener("load", init, false );
+	imgSprite.addEventListener("load", init, false);
 
 
 function init() {
@@ -64,6 +64,7 @@ function randomRange (min, max) {
 }
 
 
+
 function Player() {
 	this.srcX = 0;
 	this.srcY = 600;
@@ -78,26 +79,26 @@ function Player() {
 	this.isRightKey = false;
 	this.isDownKey = false;
 	this.isLeftKey = false;
-	this.isSpaceBar = false;
-	//this.isShooting = false;
-	//var numBullets = 10;
-	//this.bullets = [];
-	//this.currentBullets = 0;
-	//for (var i = 0; i < numBullets; i++) {
-	//	this.bullets[this.bullets.length] = new Bullet();
-	//}
+	this.isSpacebar = false;
+	this.isShooting = false;
+	var numBullets = 10;
+	this.bullets = [];
+	this.currentBullet = 0;
+	for (var i = 0; i < numBullets; i++) {
+		this.bullets[this.bullets.length] = new Bullet();
+	}
 }
 
 Player.prototype.update = function () {
 	this.centerX = this.drawX + (this.width / 2);
 	this.centerY = this.drawY + (this.height / 2);
 	this.checkDirection();
-	//this.checkShooting();
-	//this.updateAllBullets();
+	this.checkShooting();
+	this.updateAllBullets();
 };
 
 Player.prototype.draw = function () {
-	//this.drawAllBullets();
+	this.drawAllBullets();
 	ctxEntities.drawImage(imgSprite, this.srcX, this.srcY, this.width, this.height, this.drawX, this.drawY, this.width, this.height);
 };
 
@@ -144,8 +145,124 @@ Player.prototype.checkObstacleCollide = function (newDrawX, newDrawY) {
 	} else {
 		return true;
 	}
-
 };
+
+
+
+Player.prototype.checkShooting = function () {
+	if (this.isSpacebar && !this.isShooting) {
+			this.isShooting = true;
+			this.bullets[this.currentBullet].fire(this.centerX, this.centerY);
+			this.currentBullet++;	
+			if (this.currentBullet >= this.bullets.length) {
+				this.currentBullet = 0;
+			}
+	} else if (!this.isSpacebar) {
+		this.isShooting = false;
+	}
+};
+
+Player.prototype.updateAllBullets = function () {
+	for (var i = 0; i < this.bullets.length; i++) {
+		if (this.bullets[i].isFlying) {
+			this.bullets[i].update();
+		}
+	}
+};
+
+Player.prototype.drawAllBullets = function () {
+	for (var i = 0; i < this.bullets.length; i++) {
+		if (this.bullets[i].isFlying) {
+			this.bullets[i].draw();
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+function Bullet() {
+	this.radius = 2;
+	this.width = this.radius * 2;
+	this.height = this.radius * 2;
+	this.drawX = 0;
+	this.drawY = 0;
+	this.isFlying = false;
+	this.xVel = 0;
+	this.yVel = 0;
+	this.speed = 6;
+}
+
+Bullet.prototype.update = function () {
+	this.drawX += this.xVel;
+	this.drawY += this.yVel;
+	this.checkHitEnemy();
+	this.checkHitObstacle();
+	this.checkOutOfBounds();
+};
+
+Bullet.prototype.draw = function () {
+	ctxEntities.fillStyle = "white";
+	ctxEntities.beginPath();
+	ctxEntities.arc(this.drawX, this.drawY, this.radius, 0, Math.PI * 2, false);
+	ctxEntities.closePath();
+	ctxEntities.fill();
+};
+
+Bullet.prototype.fire = function (startX, startY) {
+	var soundEffect = new Audio("audio/shooting.wav");
+	soundEffect.play();
+	this.drawX = startX;
+	this.drawY = startY;
+	if (player1.srcX === 0) { // Facing south
+		this.xVel = 0;
+		this.yVel = this.speed;
+	} else if (player1.srcX === 35) { // Facing north 
+		this.xVel = 0;
+		this.yVel = -this.speed;
+	} else if (player1.srcX === 70) {// Facing west
+		this.xVel = -this.speed;
+		this.yVel = 0;
+	} else if (player1.srcX === 105) { // Facing east
+		this.xVel = this.speed;
+		this.yVel = 0;
+	}
+	this.isFlying = true;
+};
+
+Bullet.prototype.recycle = function () {
+	this.isFlying = false;
+};
+
+Bullet.prototype.checkHitEnemy = function () {
+	for (var i =0; i < enemies.length; i++) {
+		if (collision(this, enemies[i]) && !enemies[i].isDead) {
+			this.recycle();
+			enemies[i].die();
+		}
+	}
+};
+
+Bullet.prototype.checkHitObstacle = function () {
+	for (var i =0; i < obstacles.length; i++) {
+		if (collision(this, obstacles[i])) {
+			this.recycle();
+		}
+	}
+};
+
+Bullet.prototype.checkOutOfBounds = function () {
+	if (outOfBounds(this, this.drawX, this.drawY)) {
+		this.recycle();
+	}
+}
+
 
 
 function Obstacle(x, y, w, h) {
@@ -176,45 +293,7 @@ function defineObstacles() {
 			new Obstacle(605, 492, 90, bushHeight)];
 }
 
-function checkKey(e, value) {
-	var keyID = e.keyCode || e.which;
-	if (keyID === 38) { // Up arrow
-		player1.isUpKey = value;
-		e.preventDefault();
-	}
-	if (keyID === 39) { // Right arrow
-		player1.isRightKey = value;
-		e.preventDefault();
-	}
-	if (keyID === 40) { // Down arrow
-		player1.isDownKey = value;
-		e.preventDefault();
-	}
-	if (keyID === 37) { // Left arrow
-		player1.isLeftKey = value;
-		e.preventDefault();
-	}
-		if (keyID === 32 ) { // Spacebar
-		player1.isSpaceBar = value;
-		e.preventDefault();
-	}
 
-}
-
-function outOfBounds(a, x, y) { //a is object then x y positions 
-	var newBottomY = y + a.height,
-		newTopY = y,
-		newRightX = x + a.width,
-		newLeftX = x,
-		treeLineTop = 5,
-		treeLineBottom = 570,
-		treeLineRight = 750,
-		treeLineLeft = 65;
-	return newBottomY > treeLineBottom ||
-		newTopY < treeLineTop ||
-		newRightX > treeLineRight ||
-		newLeftX < treeLineLeft;
-}
 
 function Enemy() {
 	this.srcX = 140;
@@ -226,8 +305,8 @@ function Enemy() {
 	this.centerX = this.drawX + (this.width / 2);
 	this.centerY = this.drawY + (this.height / 2);
 	this.targetX = this.centerX;
-	this.tragetY = this.centerY;
-	this.randomMoveTime = randomRange(4000, 10000);
+	this.targetY = this.centerY;
+	this.randomMoveTime = randomRange(2000, 8000);
 	this.speed = 1;
 	var that = this;
 	this.moveInterval = setInterval(function() {that.setTargetLocation();}, that.randomMoveTime);
@@ -240,31 +319,12 @@ Enemy.prototype.update = function () {
 	this.checkDirection();
 };
 
-
 Enemy.prototype.draw = function () {
 	ctxEntities.drawImage(imgSprite, this.srcX, this.srcY, this.width, this.height, this.drawX, this.drawY, this.width, this.height);
 };
 
-function initEnemies() {
-	for(var i = 0; i < numEnemies; i++) {
-		enemies[enemies.length] = new Enemy();
-	}
-}
-
-function updateAllEnemies() {
-	for(var i = 0; i < enemies.length; i++) {
-		enemies[i].update();
-	}
-}
-
-function drawAllEnemies() {
-	for(var i = 0; i < enemies.length; i++) {
-		enemies[i].draw();
-	}
-}
-
 Enemy.prototype.setTargetLocation = function () {
-	this.randomMoveTime = randomRange(4000, 10000);
+	this.randomMoveTime = randomRange(2000, 8000);
 	var minX = this.centerX - 50,
 		maxX = this.centerX + 50,
 		minY = this.centerY - 50,
@@ -291,7 +351,6 @@ Enemy.prototype.checkDirection = function () {
 	} else if (this.centerX > this.targetX) {
 		this.drawX -= this.speed;
 	}
-
 	if (this.centerY < this.targetY){
 		this.drawY += this.speed;
 	} else if (this.centerY > this.targetY) {
@@ -308,5 +367,70 @@ Enemy.prototype.die = function () {
 
 };
 
+function initEnemies() {
+	for (var i = 0; i < numEnemies; i++) {
+		enemies[enemies.length] = new Enemy();
+	}
+}
+
+function updateAllEnemies() {
+	for (var i = 0; i < enemies.length; i++) {
+		enemies[i].update();
+	}
+}
+
+function drawAllEnemies() {
+	for (var i = 0; i < enemies.length; i++) {
+		enemies[i].draw();
+	}
+}
 
 
+
+
+
+function checkKey(e, value) {
+	var keyID = e.keyCode || e.which;
+	if (keyID === 38) { // Up arrow
+		player1.isUpKey = value;
+		e.preventDefault();
+	}
+	if (keyID === 39) { // Right arrow
+		player1.isRightKey = value;
+		e.preventDefault();
+	}
+	if (keyID === 40) { // Down arrow
+		player1.isDownKey = value;
+		e.preventDefault();
+	}
+	if (keyID === 37) { // Left arrow
+		player1.isLeftKey = value;
+		e.preventDefault();
+	}
+	if (keyID === 32 ) { // spacebar
+		player1.isSpacebar = value;
+		e.preventDefault();
+	}
+}
+
+function outOfBounds(a, x, y) { //a is object then x y positions 
+	var newBottomY = y + a.height,
+		newTopY = y,
+		newRightX = x + a.width,
+		newLeftX = x,
+		treeLineTop = 5,
+		treeLineBottom = 570,
+		treeLineRight = 750,
+		treeLineLeft = 65;
+	return newBottomY > treeLineBottom ||
+		newTopY < treeLineTop ||
+		newRightX > treeLineRight ||
+		newLeftX < treeLineLeft;
+}
+
+function collision(a, b) { // takes two objects enemy and bullet 
+	return a.drawX <= b.drawX + b.width &&
+		a.drawX >= b.drawX &&
+		a.drawY <= b.drawY + b.height &&
+		a.drawY >= b.drawY;
+}
